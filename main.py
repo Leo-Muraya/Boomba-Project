30de66ea796be8ad38cb093144aa7f3d4545a76e
Jeff-tech67 <geofreytech@yahoo.com>
2026-06-21 12:12:14 +0300

    changed ui to home, added an icon for the app, adjusted the frames

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+from tkinter import *
+import os
 
 
 #setting the appearance and the theme
@@ -7,8 +9,11 @@ ctk.set_default_color_theme("blue")
 
 #main window
 app = ctk.CTk()
-app.title("Music Player")
+app.title("Boomba FM")
 app.geometry("1100x600")
+
+app.iconbitmap("logo.ico")
+
 
 #dividing the space in the window
 
@@ -19,7 +24,7 @@ app.grid_columnconfigure(1, weight= 1)
 app.grid_columnconfigure(2,weight=0)
 
 #sidebar
-sidebar = ctk.CTkFrame(master = app, width=180, fg_color = "#1a1a2e")
+sidebar = ctk.CTkFrame(master = app, width=250, fg_color = "#1a1a2e")
 sidebar.grid(row=0 ,column =0 , sticky="nsew", padx= (10,5), pady=5)
 
 #main area
@@ -28,7 +33,7 @@ main_area= ctk.CTkFrame(master= app, fg_color="#1a1a2e")
 main_area.grid(row=0, column=1, sticky= "nsew",padx=5, pady=5)
 
 #visualizer panel
-visualizer=ctk.CTkFrame(master= app, width=220, fg_color= "#1a1a2e")
+visualizer=ctk.CTkFrame(master= app, width=250, fg_color= "#1a1a2e")
 visualizer.grid(row=0 , column=2, sticky= "nsew",padx=5,pady=5)
 
 
