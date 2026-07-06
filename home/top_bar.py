127f11ab0a8d45e7deae88d5f6aa17323fa65a79
Jeff-tech67 <geofreytech@yahoo.com>
2026-07-07 00:52:32 +0300

    login button working

--- home/top_bar.py
+++ home/top_bar.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 
+
 class TopBar:
     def __init__(self, parent):
         self.on_search = None
@@ -55,18 +56,7 @@ class TopBar:
         self.search_entry.bind("<KeyRelease>", self._on_search)
 
         # Login button on the right
-        Register_btn = ctk.CTkButton(
-            self.frame,
-            text="Register",
-            width=80,
-            height=35,
-            fg_color="#2563eb",
-            hover_color="#2a2a4a",
-            border_width=1,
-            border_color="#4a90d9",
-            corner_radius=20
-        )
-        Register_btn.pack(side="right", padx=5)
+       
         
         login_btn = ctk.CTkButton(
             self.frame,
@@ -77,11 +67,16 @@ class TopBar:
             hover_color="#2a2a4a",
             border_width=1,
             border_color="#4a90d9",
-            corner_radius=20
+            corner_radius=20,
+            command=self._open_login
         )
         login_btn.pack(side="right", padx=5)
+
+    def _open_login(self):
+        from login import show_login
+        show_login()
         
-    def _on_search(self, event):
+    def _on_search(self,):
      if self.on_search:
         query = self.search_entry.get()
         self.on_search(query)
